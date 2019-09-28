abc02668474d28c52b3e24c6ead9d346f5283201
Akash <41006458+akashdev99@users.noreply.github.com>
2019-09-28 21:41:53 +0400

    [FIX]

--- camera.py
+++ camera.py
@@ -3,6 +3,6 @@ from time import time
 class Camera(object):
     def __init__(self):
         self.frames = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
-
+        #https://stackoverflow.com/questions/49939859/flask-video-stream-using-opencv-images
     def get_frame(self):
         return self.frames[int(time()) % 3]
