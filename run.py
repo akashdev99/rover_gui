84e6081451dc64a4f033c2d1ac387b992f5458d9
Akash <41006458+akashdev99@users.noreply.github.com>
2019-09-28 09:09:40 +0400

    [VIDEO STREAMING] test

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,Response
 import datetime
+from camera import Camera
 
 app = Flask(__name__)
 
@@ -13,5 +14,15 @@ def index():
       }
    return render_template('hello.html',**templateData)
 
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
     app.run(debug=True)
